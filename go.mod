5db95e289dce532799db0fbc5e25e654bfa9f806
Titus Tienaah <titus.intdxdt@gmail.com>
2020-04-20 09:42:23 +0000

    added go mod

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/intdxdt/rtree
+
+go 1.13
+
+require (
+	github.com/franela/goblin v0.0.0-20200409142057-1def193310bb
+	github.com/intdxdt/cmp v0.0.0-20171102110413-58dfeb370ea5 // indirect
+	github.com/intdxdt/heap v0.0.0-20180830201411-bfa61463ae8b
+	github.com/intdxdt/math v1.0.0
+	github.com/intdxdt/mbr v1.0.0
+)
--- knn_test.go
+++ knn_test.go
@@ -1,15 +1,15 @@
 package rtree
 
 import (
-	"time"
-	"testing"
-	"github.com/intdxdt/mbr"
 	"github.com/franela/goblin"
+	"github.com/intdxdt/mbr"
+	"testing"
+	"time"
 )
 
 var knnData []BoxObject
 
-func scoreFn(query *mbr.MBR, boxer  *KObj) float64 {
+func scoreFn(query *mbr.MBR, boxer *KObj) float64 {
 	return query.Distance(boxer.MBR)
 }
 
@@ -54,7 +54,7 @@ func TestRtreeKNN(t *testing.T) {
 		g.It("finds n neighbours", func() {
 			var rt = NewRTree(9)
 			rt.Load(knnData)
-			var  nn = rt.Knn(mbr.CreateMBR(40, 40, 40, 40), 10, scoreFn)
+			var nn = rt.Knn(mbr.CreateMBR(40, 40, 40, 40), 10, scoreFn)
 			var result = []mbr.MBR{
 				{38, 39, 39, 39}, {35, 39, 38, 40}, {34, 43, 36, 44},
 				{29, 42, 33, 42}, {48, 38, 48, 40}, {31, 47, 33, 50},
@@ -135,7 +135,7 @@ func TestQobj_String(t *testing.T) {
 	g.Describe("", func() {
 		g.It("test qobject", func() {
 			var box = mbr.MBR{3, 3, 3, 3}
-			var nd  = newLeafNode(&box)
+			var nd = newLeafNode(&box)
 			var qo = &KObj{&nd, &box, true, 3.4}
 			g.Assert(box.String() + " -> 3.4").Equal(qo.String())
 		})
